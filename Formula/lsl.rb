d70c5891dee775c16dacbafc2477d3e42cc6ee65
dangomushi <dangomushi150@gmail.com>
2024-07-04 12:10:26 +0900

    update

--- Formula/lsl.rb
+++ Formula/lsl.rb
@@ -1,11 +1,11 @@
 class Lsl < Formula
   desc "tui file browser tool, written in Rust🦀"
-  homepage "https://github.com/Dangornushi/lsl.git"
+  homepage "https://github.com/Dangornushi/lsl/releases/tag/lsl-0.1.2"
   url "https://github.com/Dangornushi/lsl/releases/download/lsl-0.1.2/lsl-0.1.2-aarch64-apple-darwin.tar.gz"
   sha256 "fdcddf1a7c913aa5404049af79f5d24aabc339ed6b0b5e5ee95ae232ee361e7c"
   version "0.1.2"
 
   def install
-    bin.install "lsl"
+    bin.install "lsl2"
   end
 end
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# homebrew-lsl git init
-# homebrew-lsl
 # homebrew-lsl
